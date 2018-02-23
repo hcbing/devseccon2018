a147a8679d0f170f976e974ad9697248eb9e3a65
Alex Chan <alex_chan_binglee@apple.com>
2018-02-23 14:13:07 +0800

    commented out mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://broodkal:chan8888@ds147518.mlab.com:47518/devseccon2018
+#export MONGODB_CREDENTIALS=mongodb://gnibch:gnibch8888@ds147518.mlab.com:47518/devseccon2018
 
 # finally, run the app in dev mode...
 npm run dev
